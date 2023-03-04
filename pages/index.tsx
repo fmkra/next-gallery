6fb58c98be22cf2002a2bf9190098e67150f48b8
kotmawszy <kotmawszy@gmail.com>
2023-03-04 23:33:06 +0100

    Fix useMemo dependency array

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function() {
+export default function IndexPage() {
     return (
         <div>
             <h1>Examples</h1>
