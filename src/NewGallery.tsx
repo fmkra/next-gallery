054cf129d32be832c776b6fefe56e5411507cb13
Filip Krawczyk <kotmawszy@gmail.com>
2023-08-23 08:41:04 +0200

    Add layout changing at different screen widths

--- src/NewGallery.tsx
+++ src/NewGallery.tsx
@@ -1,3 +1,4 @@
+import { useId } from 'react'
 import type { Image as ImageT } from './Gallery'
 import Image from 'next/image'
 
@@ -31,6 +32,21 @@ const f = (ratios: number[], images: ImageT[], spanLastRow = 0) => {
     return [sizes, wl] as const
 }
 
+const containerStyle = {
+    display: `flex`,
+    flexWrap: `wrap`,
+} as const
+
+const elementStyle = (aspectRatio: number, sizes: number[]) =>
+    sizes.reduce((acc, val, idx) => ((acc[`--next-gallery-${idx + 1}`] = `${val}%`), acc), {
+        '--next-gallery-ar': `${aspectRatio}`,
+        position: 'relative',
+        boxSizing: `border-box`,
+        flexShrink: 0,
+        flexGrow: 1,
+        outline: `solid 1px red`,
+    } as Record<string, any>)
+
 export function NewGallery() {
     const widths = [500, 1000, 1600]
     const ratios = [2.2, 4, 6, 8]
@@ -56,37 +72,34 @@ export function NewGallery() {
     ] as ImageT[]
     const [sizes, width_left] = f(ratios, images)
 
+    const id = useId().replace(/:/g, '')
+
     return (
         <>
-            <style>{`
-                .next-gallery__container {
-                    display: flex;
-                    flex-wrap: wrap;
-                }
-                .next-gallery__element {
-                    position: relative;
-                    box-sizing: border-box;
-                    flex-shrink: 0;
-                    flex-grow: 1;
-                    outline: solid 1px red;
-                    width: var(--gallery-4);
-                    padding-bottom: calc(var(--gallery-4) / var(--aspect-ratio));
-                }
-            `}</style>
-            <div className="next-gallery__container">
+            <style>
+                {`
+                .next-gallery__element-${id} {
+                    width: var(--next-gallery-1);
+                    padding-bottom: calc(var(--next-gallery-1) / var(--next-gallery-ar));
+                }` +
+                    widths
+                        .map(
+                            (width, i) =>
+                                `@media (min-width: ${width}px) {
+                                .next-gallery__element-${id} {
+                                    width: var(--next-gallery-${i + 2});
+                                    padding-bottom: calc(var(--next-gallery-${i + 2}) / var(--next-gallery-ar));
+                                }
+                            }`
+                        )
+                        .join('')}
+            </style>
+            <div style={containerStyle}>
                 {sizes.map((size, i) => (
                     <div
-                        className="next-gallery__element"
+                        className={`next-gallery__element-${id}`}
                         key={i}
-                        style={size.reduce(
-                            (acc, val, idx) => {
-                                acc[`--gallery-${idx + 1}`] = `${val}%`
-                                return acc
-                            },
-                            {
-                                '--aspect-ratio': `${images[i].aspect_ratio}`,
-                            } as Record<string, string>
-                        )}
+                        style={elementStyle(images[i].aspect_ratio, size)}
                     >
                         <Image
                             src={images[i].src}
