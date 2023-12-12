3cd14371989dfea9fca97e1eaf200ae89dce38c5
Filip Krawczyk <kotmawszy@gmail.com>
2023-12-12 12:33:59 +0100

    Rebuild

--- dist/types/GalleryElement.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { GalleryCalculationProps } from './calculateImageSizes';
-type GalleryProps = GalleryCalculationProps & {
-    widths: number[];
-    children: (index: number) => React.ReactNode;
-};
-export declare function GalleryElement({ widths, children, ...props }: GalleryProps): import("react/jsx-runtime").JSX.Element;
-export {};
